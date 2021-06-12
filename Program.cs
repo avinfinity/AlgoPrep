0614bd17bda30ba791ba97f46ff94f4671263669
Avinash Rai <avirai1@publicisgroupe.net>
2021-06-12 09:36:27 +0530

    added

--- Program.cs
+++ Program.cs
@@ -2,18 +2,27 @@
 
 namespace AlgoPrep
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            UniquenessCheck.Verify();
+            // UniquenessCheck.Verify();
 
-            Console.WriteLine("Reversed".Reverse());
-            Console.WriteLine("".Reverse());
-            Console.WriteLine(string.Empty.Reverse());
+            // Console.WriteLine("Reversed".Reverse());
+            // Console.WriteLine("".Reverse());
+            // Console.WriteLine(string.Empty.Reverse());
 
-            Assert.IsTrue("Reversed".Reverse().Reverse() == "Reversed");
+            // Assert.IsTrue("Reversed".Reverse().Reverse() == "Reversed");
 
+            MyQueue queue = new MyQueue();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+
+          Console.WriteLine(queue.Dequeue());
+           Console.WriteLine(queue.Dequeue());
+           Console.WriteLine(queue.Dequeue());
         }
     }
 }
